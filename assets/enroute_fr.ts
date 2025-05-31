dcddf4175ffb1e80993d52e040190e1bcec2477c
Lukesw74 <riva.luca@gmail.com>
2025-05-31 22:03:08 +0200

    update french translations

--- assets/enroute_fr.ts
+++ assets/enroute_fr.ts
@@ -2575,7 +2575,7 @@ Message: Enroute Flight Navigation
     <message>
         <location filename="../../../src/Librarian.cpp" line="303"/>
         <source>Enroute Flight Navigation is now available on Mac computers with Apple Silcon processor (M1 or newer). Download it on the App Store.</source>
-        <translation type="unfinished"></translation>
+        <translation>Enroute Flight Navigation est désormais disponible pour les ordinateurs Mac équipés d&apos;un processeur Apple Silcon (M1 ou supérieur). Téléchargez-la sur l&apos;App Store.</translation>
     </message>
     <message>
         <source>Enroute Flight Navigation now offers ICAO and Glider Charts for Switzerland. To download these maps, open the main menu and go to Library/Maps and Data. We thank the swiss Federal Office of Topography and the Federal Office of Civil Aviation for making the maps publicly available. Use these maps for information only. The &lt;a href=&apos;https://www.geo.admin.ch/en/general-terms-of-use-fsdi&apos;&gt;license conditions&lt;/a&gt; do not allow operational use.</source>
